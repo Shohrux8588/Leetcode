3135a3688c504797de29ae2beb4db5174c2d9d3b
Shohrux8588 <70914015+Shohrux8588@users.noreply.github.com>
2021-11-15 13:37:49 +0500

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,1 +1,20 @@
 // the first solution
+1534. Count Good Triplets
+var countGoodTriplets = function(arr, a, b, c) {
+  let triplets = 0;  
+  for(let i = 0; i<arr.length-2; i++){
+      for(let j = i+1; j<arr.length-1; j++){
+        for(let k = j+1; k<arr.length; k++){
+          if(Math.abs(arr[i]-arr[j])<=a){
+            if(Math.abs(arr[j]-arr[k])<=b){
+              if(Math.abs(arr[i]-arr[k])<=c){
+                triplets++;
+              }
+            }
+          }
+        }
+      }
+    }
+  return triplets;
+}
+
