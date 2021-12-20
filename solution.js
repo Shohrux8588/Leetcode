ff93b932e56d5cdf92982ec62d8b6aed909caa81
Shohrux8588 <70914015+Shohrux8588@users.noreply.github.com>
2021-12-20 12:46:29 +0500

    Update solution.js

--- solution.js
+++ solution.js
@@ -118,3 +118,18 @@ var findTheDifference = function(s, t) {
         }
     }
 };
+
+1952. Three Divisors
+var isThree = function(n) {
+    let numberOfDivisors = 0;
+    for (let i = 1; i <= n; i++) {
+        if (n % i === 0) {
+            numberOfDivisors++;
+        }
+        if(numberOfDivisors>=3){
+            return true;
+        }
+    }
+    return false;
+
+};
