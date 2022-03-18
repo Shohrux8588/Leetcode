19d4c47c175ccb500bbf5e1a1f6ab76131f795d6
Shohrux8588 <70914015+Shohrux8588@users.noreply.github.com>
2022-03-18 12:18:39 +0500

    Update solution.js

--- solution.js
+++ solution.js
@@ -33,6 +33,397 @@ var judgeCircle = function (m) {
         sum[1] += coordinate[1];
         return sum;
     }, [0, 0]);
+  
+  var modifyString = function(s) {
+   	let letters = "abcdefghijklmnopqrstuvwxyz";
+   	let index = 0;
+   	let arr = s.split("");
+   	arr = arr.map((e, i) => {
+   		if(e!=="?"){
+   			return e;
+   		}
+        if(index===25){
+            index=0;
+        }
+        index++;
+   		while(arr[i-1]===letters[index]||arr[i+1]===letters[index]){
+   			index++;
+            if(index===25){
+                index = 0;
+            }
+   		}
+   		
+   		return letters[index];
+   	})
+   	return arr.join("");
+};
+const s1 = "??zs";
+const s2 = "ubv?w";
+
+console.log(modifyString(s1));
+console.log(modifyString(s2));
+
+
+var majorityElement = function(nums) {
+    const obj = nums.reduce((acc, num)=>{
+    	if(acc.hasOwnProperty(num)){
+    		acc[num]++;
+    	}else{
+    		acc[num] = 1;
+    	}
+    	return acc;
+    },{})
+    const max = Math.max(...Object.values(obj));
+    for(let [key, value] of Object.entries(obj)){
+    	if(value === max){
+    		return Number(key);
+    	}
+    }
+};
+
+const nums = [2, 2, 1, 1, 1, 2, 2];
+console.log(majorityElement(nums));
+
+var freqAlphabets = function(s) {
+    const alphabet = ['','a', 'b', 'c','d', 'e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+    let result = '';
+    for(let i = 0; i < s.length; i++){
+    	if(s[i + 2] === '#'){
+    		result+=alphabet[s[i]+s[i+1]];
+    		i+=2;
+    	}else{
+    		result+=alphabet[s[i]];
+    	}
+    }
+    return result;
+};
+
+const s1 = "10#11#12";
+const s2 = "1326#";
+freqAlphabets(s1);
+freqAlphabets(s2);
+
+
+var getNoZeroIntegers = function(n) {
+    let i = 1;
+    while(String(n-i).includes('0')||String(i).includes('0')){
+    	i++;
+    }
+    console.log([i, n-i]);
+    return [i, n-i];
+};
+
+getNoZeroIntegers(2);
+getNoZeroIntegers(11);
+
+var countBits = function(n) {
+	let result = [];
+	for(let i = 0; i<=n; i++){
+		let arrOfBits = i.toString(2).match(/1/gm);
+		result.push(arrOfBits ? arrOfBits.length : 0 );
+	}    
+	return result;
+};
+
+countBits(5);
+
+var isSubsequence = function(s, t) {
+	let startIndex = 0, hasChar = false;
+	for(let i = 0; i < s.length; i++){
+		hasChar = false;
+		for(let j = startIndex; j < t.length; j++){
+			if(s[i] === t[j]){
+				startIndex = j + 1;
+				j = t.length;
+				hasChar = true;
+			}
+		}
+		if(!hasChar){
+			return false;
+		}
+	}
+	return true;
+};
+
+const s = "aaaaaa";
+const t = "bbaaaa";
+console.log(isSubsequence(s, t));
+
+var shortestToChar = function(s, c) {
+	let indexes = [], res = [], min;
+	const len = s.length;
+    for(let i = 0; i<len; i++){
+    	if(s[i]===c){
+    		indexes.push(i);
+    	}
+    }
+
+    for(let i = 0; i<len;i++){
+    	min = Math.min.apply(null, indexes.map(index=>Math.abs(index-i)));
+    	res.push(min);
+    }
+
+	return res;
+};
+
+const s = "loveleetcode";
+const c = "e";
+shortestToChar(s,c);
+
+
+var countGoodSubstrings = function(s) {
+	let counter = 0, substring;
+	s = s.split('');
+    for(let i = 0; i<s.length-2; i++){
+    	substring = s.slice(i, i + 3);
+    	if(substring.length===[...new Set(substring)].length){
+    		counter++;
+    	}
+    }
+    return counter;
+};
+
+const s = "xyzzaz";
+countGoodSubstrings(s);
+
+var findLengthOfLCIS = function(nums) {
+	let longest = 0; counter = 0;
+    for(let i = 0; i < nums.length; i++){
+    	counter = 1;
+    	for(let j = i; j < nums.length; j++){
+    		if(nums[j] < nums[j+1]){
+    			counter++;
+    		} else {
+    			j = nums.length;
+    		}
+            if(longest < counter){
+                longest = counter;
+            }
+    	}
+    }
+    return longest;
+};
+
+const nums = [1,3,5,4,7];
+const nums1 = [2,2,2,2,2];
+const nums2 = [1,2,3,4];
+const nums3 = [1,2];
+console.log(findLengthOfLCIS(nums));
+console.log(findLengthOfLCIS(nums1));
+console.log(findLengthOfLCIS(nums2));
+console.log(findLengthOfLCIS(nums3));
+
+var divisorGame = function(n) {
+	let turn = 0;
+	while(n !== 1){
+		turn++;
+        n = n - 1;
+	}
+    return turn % 2 === 1;
+};
+
+console.log(divisorGame(2));
+
+var checkRecord = function(s) {
+    return numberOfAbsentDays(s) < 2 || hasNotConsecutiveLateThreeDays(s); 
+};
+
+function numberOfAbsentDays(s){
+	return s.match(/A/gm) ? s.match(/A/gm).length : 0;
+}
+
+function hasNotConsecutiveLateThreeDays(s) {
+	for(let i = 0; i < s.length; i++){
+		if(s[i]==='L' && s[i + 1] === 'L' && s[i + 2] === 'L'){
+			return false;
+		}
+	}
+	return true;
+}
+
+const s1 = "PPALLP";
+checkRecord(s1);
+
+var rotateString = function(s, goal) {
+    for(let i = 1; i < s.length; i++){
+    	if(goal === s.slice(i) + s.slice(0, i)){
+    		return true;
+    	}
+    }
+    return false;
+};
+
+const s = "abcde";
+const goal = "cdeab";
+
+rotateString(s, goal);
+
+
+var buddyStrings = function(s, goal) {
+    if(s.length !== goal.length){
+        return false;
+    }
+    if(s !== goal){
+        let differenceCounter = 0, differences = [];
+        for(let i = 0; i < s.length; i++){
+            if(s[i] !== goal[i]){
+                differenceCounter++;
+                differences.push(s[i], goal[i]);
+            }
+        }
+        if(differenceCounter === 2){
+        	if(differences[0]===differences[3]&&differences[1]===differences[2]){
+        		return true;
+        	}
+        	return false;
+        }
+        return false;
+    }else{
+    	const uniqueLetters = [...new Set(s)];
+    	console.log(uniqueLetters)
+    	return uniqueLetters.length !== s.length;
+    }
+};
+
+const s = 'ab';
+const goal = 'ab';
+console.log(buddyStrings(s, goal));
+
+var areAlmostEqual = function(s1, s2) {
+	if(s1===s2){
+		return true;
+	}
+	if(s1.length!==s2.length){
+		return false;
+	}
+    let differenceCounter = 0, differences = [];
+    for(let i = 0; i < s1.length; i++){
+            if(s[i] !== goal[i]){
+                differenceCounter++;
+                differences.push(s1[i], s2[i]);
+            }
+        }
+        if(differenceCounter === 2){
+        	if(differences[0]===differences[3]&&differences[1]===differences[2]){
+        		return true;
+        	}
+        	return false;
+        }
+        return false;
+};
+
+
+var lastStoneWeight = function (stones) {
+  let sortedStones = stones.sort((a, b) => b - a),
+    maxStone1,
+    maxStone2;
+  while (sortedStones.length > 1) {
+    console.log(sortedStones);
+    maxStone1 = sortedStones[0];
+    maxStone2 = sortedStones[1];
+    sortedStones = sortedStones.slice(2);
+    if (maxStone1 > maxStone2) {
+      sortedStones = [...sortedStones, maxStone1 - maxStone2].sort(
+        (a, b) => b - a
+      );
+    }
+  }
+  return sortedStones.length > 0 ? sortedStones[0] : 0;
+};
+
+
+Minimum Number of Steps to Make Two Strings Anagram I
+var minSteps = function(s, t) {
+	let steps = 0;
+    let frequencyCounter1 = {};
+    let frequencyCounter2 = {};
+
+    for(let char of s){
+    	if(!frequencyCounter1.hasOwnProperty(char)){
+    		frequencyCounter1[char] = 0;
+    	}
+    	frequencyCounter1[char]++;
+    }
+
+    for(let char of t){
+    	if(!frequencyCounter2.hasOwnProperty(char)){
+    		frequencyCounter2[char] = 0;
+    	}
+    	frequencyCounter2[char]++;
+    }
+
+    for(let key of Object.keys(frequencyCounter1)){
+    	if(frequencyCounter2[key]<frequencyCounter1[key]){
+    		steps += frequencyCounter1[key] - frequencyCounter2[key];
+    	}else if(!frequencyCounter2[key]){
+    		steps += frequencyCounter1[key];
+    	}
+    }
+    return steps;
+};
+
+const s = "bab";
+const t = "aba";
+
+const s1 = "leetcode"
+const t1 = "practice"
+
+const s2 = 'anagram';
+const t2 = 'mangaar';
+
+minSteps(s, t);
+minSteps(s1, t1);
+minSteps(s2, t2);
+
+Minimum Number of Steps to Make Two Strings Anagram II
+var minSteps = function(s, t) {
+	let stepsCounter = 0;
+    let frequencyCounter1 = {};
+    let frequencyCounter2 = {};
+
+    for(let char of s){
+    	if(!frequencyCounter1.hasOwnProperty(char)){
+    		frequencyCounter1[char] = 0;
+    	}
+    	frequencyCounter1[char]++;
+    }
+
+    for(let char of t){
+    	if(!frequencyCounter2.hasOwnProperty(char)){
+    		frequencyCounter2[char] = 0;
+    	}
+    	frequencyCounter2[char]++;
+    }
+
+    for(let key of Object.keys(frequencyCounter1)){
+    	if(!frequencyCounter2[key]){
+    		stepsCounter += frequencyCounter1[key];
+    	}
+    	if(frequencyCounter2[key]<frequencyCounter1[key]){
+    		stepsCounter += frequencyCounter1[key] - frequencyCounter2[key];
+    	}
+    }
+    for(let key of Object.keys(frequencyCounter2)){
+    	if(!frequencyCounter1[key]){
+    		stepsCounter += frequencyCounter2[key];
+    	}
+    	if(frequencyCounter1[key]<frequencyCounter2[key]){
+    		stepsCounter += frequencyCounter2[key] - frequencyCounter1[key];
+    	}
+    }
+    console.log(stepsCounter);
+    return stepsCounter;
+};
+
+const s = "leetcode";
+const t = "coats";
+
+const s1 = 'night';
+const t1 = 'thing';
+
+minSteps(s, t);
+minSteps(s1, t1);
+
     return result[0] === 0 && result[1] === 0;
 };
 
